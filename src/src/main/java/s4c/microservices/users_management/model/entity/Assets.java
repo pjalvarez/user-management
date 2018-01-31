d040ccf0eeab0ed721c69e0fce104ea8b2326a5b
Rafael Carballo <rcarballo@emergya.com>
2018-01-31 13:08:04 +0100

    Dummie change

--- src/src/main/java/s4c/microservices/users_management/model/entity/Assets.java
+++ src/src/main/java/s4c/microservices/users_management/model/entity/Assets.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name = "assets")
 public class Assets {
-	
+//dummie change	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
